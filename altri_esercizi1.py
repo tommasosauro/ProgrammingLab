e171fdbe9158ea9298e14a794d07df9285f643c7
Tommaso Sauro <tommaso.sauro05@gmail.com>
2025-12-03 12:09:48 +0100

    Organizzazione Cartelle

---
+++ altri_esercizi1.py
@@ -0,0 +1,17 @@
+#Altri esercizi sulla liberia Numpy
+
+import numpy as np
+
+#Esercizio 1
+
+array_zeri = np.zeros(10)
+print(array_zeri)
+
+array_uni = np.ones((3,3))
+print(array_uni)
+
+array_1 = np.linspace(1,9,9)
+print(array_1)
+
+array_identita = np.eye(4,4)
+print(array_identita)
